e9ebc6d6daa768df43ba3a82ce56b056c45ca127
andrestorres <andrestorresandres45@hotmail.com>
2023-05-11 18:25:58 -0500

    ejercicio 10

---
+++ controller.js
@@ -0,0 +1,12 @@
+export {suma , multiplicar};
+
+function suma(num1, num2){
+   let sum = num1 + num2;  
+   return sum
+}
+
+function multiplicar(num1,num2){
+  let mult = num1 * num2
+  return mult
+}
+
---
+++ index.js
@@ -0,0 +1,5 @@
+import { suma, multiplicar } from "./controller.js";
+import chalk from 'chalk';
+
+console.log(suma(2,5));
+console.log(chalk.green(multiplicar(10,2)));
